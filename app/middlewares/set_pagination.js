908c1f9298b9a47c791c55809cbeec10a38f0000
Karan0009 <Karansinghk0@gmail.com>
2025-03-12 12:38:18 +0530

    fix: minor fixes

--- app/middlewares/set_pagination.js
+++ app/middlewares/set_pagination.js
@@ -1,9 +1,3 @@
-import jwt from '../lib/jwt.js';
-import userService from '../services/user_service.js';
-import { HttpStatusCode } from 'axios';
-import config from '../config/config.js';
-import createHttpError from 'http-errors';
-
 /**
  *
  * @param {import('express').Request} req
