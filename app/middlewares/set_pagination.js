6487bd788494572dd18a21b8c4b94358f69babff
Karan0009 <Karansinghk0@gmail.com>
2025-04-13 18:47:49 +0530

    fix: pagination error

--- app/middlewares/set_pagination.js
+++ app/middlewares/set_pagination.js
@@ -1,3 +1,5 @@
+import config from '../config/config.js';
+
 /**
  *
  * @param {import('express').Request} req
@@ -8,6 +10,12 @@ const setPagination = async (req, _, next) => {
   try {
     let limit = parseInt(req.query.limit);
     let page = parseInt(req.query.page);
+    if (isNaN(limit)) {
+      limit = config.DEFAULT_GET_REQUEST_LIMIT;
+    }
+    if (isNaN(page)) {
+      page = 1;
+    }
     let offset = (page - 1) * limit;
     req.query.limit = limit;
     req.query.offset = offset;
