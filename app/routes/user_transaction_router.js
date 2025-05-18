e992b1defe106c5097b62b521637272249e2b822
Karan0009 <Karansinghk0@gmail.com>
2025-05-18 15:19:57 +0530

    fix: refactor summary route handler for user transactions to improve validation logic

--- app/routes/user_transaction_router.js
+++ app/routes/user_transaction_router.js
@@ -10,6 +10,61 @@ import { HttpStatusCode } from 'axios';
 const userTransactionsRouter = express.Router();
 
 userTransactionsRouter.get(
+  '/summary',
+  query('summary_type').isIn(Object.values(config.SUMMARY_TYPE)).escape(),
+  query('sub_cat_id').isNumeric().optional(),
+  query('group_by').isString().optional(),
+  oneOf(
+    [
+      query('on_date')
+        .matches(/^\d{4}-\d{2}-\d{2}$/)
+        .exists()
+        .notEmpty()
+        .escape(),
+      [
+        query('from_date')
+          .matches(/^\d{4}-\d{2}-\d{2}$/)
+          .exists()
+          .notEmpty()
+          .escape(),
+        query('to_date')
+          .matches(/^\d{4}-\d{2}-\d{2}$/)
+          .exists()
+          .notEmpty()
+          .escape(),
+      ],
+    ],
+    {
+      errorType: 'flat',
+      message: 'Either on_date or from_date and to_date must be provided',
+    },
+  ),
+  (req, _, next) => {
+    try {
+      const {
+        on_date: onDate,
+        from_date: fromDate,
+        to_date: toDate,
+      } = req.query;
+      if (onDate && (fromDate || toDate)) {
+        throw createHttpError(HttpStatusCode.BadRequest, {
+          errors: 'Either on_date or from_date and to_date must be provided',
+        });
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  ...sortOrderValidator(
+    ['amount', 'transaction_datetime'],
+    'transaction_datetime',
+    config.ORDER_BY.desc,
+  ),
+  transactionSummaryController.getSummary,
+);
+
+userTransactionsRouter.get(
   '/:id',
   param('id').isNumeric().notEmpty().escape(),
   transactionController.show,
@@ -72,60 +127,5 @@ userTransactionsRouter.put(
 );
 
 userTransactionsRouter.delete('/:id', transactionController.delete);
-
-userTransactionsRouter.get(
-  '/summary',
-  query('summary_type').isIn(Object.values(config.SUMMARY_TYPE)).escape(),
-  query('sub_cat_id').isNumeric().optional(),
-  query('group_by').isString().optional(),
-  oneOf(
-    [
-      query('on_date')
-        .matches(/^\d{4}-\d{2}-\d{2}$/)
-        .exists()
-        .notEmpty()
-        .escape(),
-      [
-        query('from_date')
-          .matches(/^\d{4}-\d{2}-\d{2}$/)
-          .exists()
-          .notEmpty()
-          .escape(),
-        query('to_date')
-          .matches(/^\d{4}-\d{2}-\d{2}$/)
-          .exists()
-          .notEmpty()
-          .escape(),
-      ],
-    ],
-    {
-      errorType: 'flat',
-      message: 'Either on_date or from_date and to_date must be provided',
-    },
-  ),
-  (req, _, next) => {
-    try {
-      const {
-        on_date: onDate,
-        from_date: fromDate,
-        to_date: toDate,
-      } = req.query;
-      if (onDate && (fromDate || toDate)) {
-        throw createHttpError(HttpStatusCode.BadRequest, {
-          errors: 'Either on_date or from_date and to_date must be provided',
-        });
-      }
-      next();
-    } catch (err) {
-      next(err);
-    }
-  },
-  ...sortOrderValidator(
-    ['amount', 'transaction_datetime'],
-    'transaction_datetime',
-    config.ORDER_BY.desc,
-  ),
-  transactionSummaryController.getSummary,
-);
 
 export default userTransactionsRouter;
